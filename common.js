adc17fce538c31bea6095f24ff4c8f80b3719b0d
gk.khan <meheduvau@gmail.com>
2024-09-23 02:57:09 +0600

    add another js file

---
+++ common.js
@@ -0,0 +1,12 @@
+function getElementByText(){
+    const textValue = document.getElementById(id).textContent
+    const textValues = parseFloat(textValue)
+    return textValues
+
+}
+
+function getElementsByInput(){
+    const inputValue = document.getElementById(id).value 
+    const inputValues = parseFloat(inputValue)
+    return inputValues
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('donation-btn').addEventListener('click' , function(){
+
+    const donate = document.getElementById('add-money-noakhali').value;
+    const mainbalanceN = document.getElementById('noakhali-donation').innerText
+    const mainBalanceN = parseFloat(mainbalanceN) ;
+    const newdonate = parseFloat(donate)
+    
+    const originalBanlance = mainBalanceN + newdonate
+
+    document.getElementById('noakhali-donation').innerText = originalBanlance + " BDT"
+})
